5d9e2484478ad17be3c8a4fbd7f725afb05bcd70
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-04-25 09:28:54 -0400

    feat: inicializacion del microservicio con Spring Boot

---
+++ Microservicio-Donaciones/src/main/java/com/donaton/donaciones/MicroservicioDonacionesApplication.java
@@ -0,0 +1,13 @@
+package com.donaton.donaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDonacionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioDonacionesApplication.class, args);
+    }
+
+}
---
+++ Microservicio-Donaciones/src/test/java/com/donaton/donaciones/MicroservicioDonacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donaton.donaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDonacionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
