9f9bc828d9ef2367619b4aaa22899af46d3b5574
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-04-26 16:27:11 -0400

    feat: Crear endpoints rest para donaciones

---
+++ Microservicio-Donaciones/src/main/java/com/donaton/donaciones/controller/DonacionController.java
@@ -0,0 +1,29 @@
+package com.donaton.donaciones.controller;
+
+import com.donaton.donaciones.model.Donacion;
+import com.donaton.donaciones.service.DonacionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/donaciones")
+public class DonacionController {
+
+    private final DonacionService service;
+
+    public DonacionController(DonacionService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Donacion crear(@RequestBody Donacion donacion) {
+        return service.guardar(donacion);
+    }
+
+    @GetMapping
+    public List<Donacion> listar() {
+        return service.listar();
+    }
+}
