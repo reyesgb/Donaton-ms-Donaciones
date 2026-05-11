db735296e556b744b98f5f9192946f712a44da21
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2026-05-11 16:56:55 -0400

    feat: integrar CRUD de donaciones, Factory Pattern, DTOs, Circuit Breaker y soporte CORS

--- Microservicio-Donaciones/src/main/java/com/donaton/donaciones/controller/DonacionController.java
+++ Microservicio-Donaciones/src/main/java/com/donaton/donaciones/controller/DonacionController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/donaciones")
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "*")
 public class DonacionController {
 
     private final DonacionService service;
